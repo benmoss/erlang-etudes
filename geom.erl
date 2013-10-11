5601094bce8387bb4d404d3f9ff001e38d1949e9
Ben Moss <ben@mossity.com>
2013-10-10 22:11:24 -0400

    Finish ch3

--- geom.erl
+++ geom.erl
@@ -4,9 +4,12 @@
 %% @version 1.0
 
 -module(geom).
--export([area/2]).
+-export([area/1]).
 
-%% @doc call this if u want area for a rectangle
--spec(area(number(), number()) -> number()).
+area({Planet, X, Y}) -> area(Planet, X, Y).
 
-area(Length, Width) -> Length * Width.
+area(rectangle, Length, Width) when Length >= 0, Width >= 0 -> Length * Width;
+area(triangle, Base, Height) when Base >= 0, Height >= 0 -> Base * Height / 2.0;
+area(ellipse, Major, Minor) when Major >= 0, Minor >= 0 -> math:pi() * Major * Minor;
+area(_, _, _) -> 0.
+
